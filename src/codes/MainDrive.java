a4365c995f477ba04b316e0473cb393588c110a3
조경진 <cho881020@gmail.com>
2021-09-25 11:45:14 +0900

    컴퓨터 출제용 배열 생성
     - for문 구조만 먼저 작성

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -7,6 +7,23 @@ public class MainDrive {
 //		숫자 야구 게임입니다.
 //		컴퓨터가 3개의 숫자를 랜덤으로 문제로 냅니다.
 		
+		int[] cpuNumbers = new int[3];
+		
+//		3자리 숫자를 채우기 위한 for
+		for (int i=0 ; i < cpuNumbers.length ; i++) {
+			
+//			1~9 숫자
+			
+//			중복 X 검사
+			
+		}
+		
+//		3자리 숫자 확인.
+		for (int cpuNum :  cpuNumbers) {
+			System.out.println(cpuNum);
+		}
+		
+		
 	}
 
 }
